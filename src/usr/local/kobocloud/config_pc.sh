83ac64f03c600d250950d06c9894b58356a36ae7
DC* <des@riseup.net>
2020-05-09 17:46:27 -0300

    Added user agent configuration to try to avoid google flagging request as automated

--- src/usr/local/kobocloud/config_pc.sh
+++ src/usr/local/kobocloud/config_pc.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-Logs=/home/francesco/src/KoboCloud
-Lib=/home/francesco/src/KoboCloud/Library
-SD=/home/francesco/src/KoboCloud
-UserConfig=/home/francesco/src/KoboCloud/kobocloudrc
+Logs=/tmp/KoboCloud
+Lib=/tmp/KoboCloud/Library
+SD=/tmp/KoboCloud
+UserConfig=/tmp/KoboCloud/kobocloudrc
 Dt="date +%Y-%m-%d_%H:%M:%S"
 CURL=/usr/bin/curl
+UserAgent="Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"
