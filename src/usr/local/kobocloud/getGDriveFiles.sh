a57aaaaec3110cce35425dcf5af01dce20d90075
DC* <des@riseup.net>
2020-05-09 17:47:42 -0300

    Add error handling from getRemoteFile
    Update logging format

--- src/usr/local/kobocloud/getGDriveFiles.sh
+++ src/usr/local/kobocloud/getGDriveFiles.sh
@@ -18,13 +18,17 @@ $CURL -k -L --silent "$baseURL" |
 grep -Eo "\\\x5b\\\x22[^\\\]*\\\x22,\\\x5b\\\x22$gDirCode\\\x22\\\x5d\\\n,\\\x22[^\\\]*" | # find links
 while read fileInfo
 do
-    echo $fileInfo
+    echo "File info: $fileInfo"
     fileCode=`echo $fileInfo | sed -n 's/x5bx22\([^\\\]*\)x22,.*/\1/p'` # extract the code for file download (this is how a file is identified in GDrive)
     fileName=`echo $fileInfo | sed -n 's/.*x22\(.*\)$/\1/p'` # extract the file name
-    echo $fileCode
-    echo $fileName
+    echo "File code: $fileCode"
+    echo "File name: $fileName"
     linkLine="https://drive.google.com/uc?id=$fileCode&export=download"
     outFileName=`echo $fileName | tr ' ' '_'`
     localFile="$outDir/$outFileName"
     `dirname $0`/getRemoteFile.sh "$linkLine" "$localFile"
+    if [ "$?" = "2" ]; then
+        echo "Google detected automated requests. Try again in a couple of minutes."
+        exit
+    fi
 done
