2c6bbb7ef9354d2c94f4b5993ec235139c5d1b38
Dario Cavuotti <dario.cavuotti@tiendanube.com>
2020-05-10 18:06:42 -0300

    Update error handling for google drive

--- src/usr/local/kobocloud/config_pc.sh
+++ src/usr/local/kobocloud/config_pc.sh
@@ -5,6 +5,6 @@ SD=/tmp/KoboCloud
 UserConfig=/tmp/KoboCloud/kobocloudrc
 Dt="date +%Y-%m-%d_%H:%M:%S"
 CURL=/usr/bin/curl
-UserAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+UserAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
 CurlExtra=""
 GdriveCurlExtra="-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US;q=0.5,en;q=0.3' --compressed -H 'Connection: keep-alive'"
--- src/usr/local/kobocloud/getRemoteFile.sh
+++ src/usr/local/kobocloud/getRemoteFile.sh
@@ -32,37 +32,40 @@ echo "  Command: '$curlHead'"
 $curlHead > $remoteInfo
 echo "  Status: $?"
 
-remoteSize=`cat $remoteInfo | tr A-Z a-z | sed -n 's/^content-length\: \([1-9]*\).*/\1/p'`
+remoteSize=`cat $remoteInfo | tr A-Z a-z | grep 'content-length:' | tail -n 1 | cut -d' ' -f2`
 statusCode=`cat $remoteInfo | grep 'HTTP/' | tail -n 1 | cut -d' ' -f2`
 echo "Remote file information:"
 echo "  Remote size: $remoteSize"
 echo "  Status code: $statusCode"
 rm "$remoteInfo"
 
-if echo "$statusCode" | grep -q "4.*"; then
+if echo "$statusCode" | grep -q "403"; then
     echo "Error: Forbidden"
     exit 2
 fi
-if echo "$statusCode" | grep -q "5.*"; then
+if echo "$statusCode" | grep -q "50.*"; then
     echo "Error: Server error"
     exit 3
 fi
 
-if [ -f $localFile ]; then
-  localSize=`stat -c%s "$localFile"`
+if [ -f "$localFile" ]; then
+    localSize=`stat -c%s "\"$localFile\""`
 else
-  localSize=0
+    localSize=0
 fi
 if [ "$remoteSize" = "" ]; then
-  remoteSize=1
+    remoteSize=1
 fi
-if [ $localSize -ge $remoteSize ]; then
-  echo "File exists: skipping"
+if [ "$localSize" -ge $remoteSize ]; then
+    echo "File exists: skipping"
 else
-  $curlCommand -k --silent -C - -L -o "$localFile" "$linkLine" # try resuming
-  if [ $? -ne 0 ]; then
-    echo "Error resuming: redownload file"
-    $curlCommand -k --silent -L -o "$localFile" "$linkLine" # restart download
-  fi
+    $curlCommand -k --silent -C - -L -o "\"$localFile\"" "$linkLine" # try resuming
+    status=$?
+    echo "Status: $?"
+    if [ $status -ne 0 ]; then
+        echo "Error resuming: redownload file ($status)"
+        $curlCommand -k --silent -L -o "\"$localFile\"" "$linkLine" # restart download
+        echo "Status: $?"
+    fi
 fi
 echo "getRemoteFile ended"
