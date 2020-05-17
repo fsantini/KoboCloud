b6c6a91fccd53dd74b2bcd632890cd545bf8ce3f
Dario Cavuotti <dario.cavuotti@tiendanube.com>
2020-05-17 17:57:59 -0300

    Remove user-agent and custom headers configuration

--- src/usr/local/kobocloud/config_kobo.sh
+++ src/usr/local/kobocloud/config_kobo.sh
@@ -5,6 +5,3 @@ SD=/mnt/sd/kobocloud
 UserConfig=/mnt/onboard/.add/kobocloud/kobocloudrc
 Dt="date +%Y-%m-%d_%H:%M:%S"
 CURL="`dirname $0`/curl --cacert \"`dirname $0`/ca-bundle.crt\" "
-UserAgent=""
-CurlExtra=""
-GdriveCurlExtra=""
--- src/usr/local/kobocloud/config_pc.sh
+++ src/usr/local/kobocloud/config_pc.sh
@@ -5,6 +5,3 @@ SD=/tmp/KoboCloud
 UserConfig=/tmp/KoboCloud/kobocloudrc
 Dt="date +%Y-%m-%d_%H:%M:%S"
 CURL=/usr/bin/curl
-UserAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
-CurlExtra=""
-GdriveCurlExtra="-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US;q=0.5,en;q=0.3' --compressed -H 'Connection: keep-alive'"
