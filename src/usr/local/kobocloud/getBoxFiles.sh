a918f8219663dece18e3d9ed53ae8d7e8ce6192a
Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
2020-12-30 13:06:18 +0000

    Added support for several pages in Box folder.

--- src/usr/local/kobocloud/getBoxFiles.sh
+++ src/usr/local/kobocloud/getBoxFiles.sh
@@ -6,34 +6,46 @@ outDir="$2"
 #load config
 . $(dirname $0)/config.sh
 
+currPage=1
+
 # get directory listing
 echo "Getting $baseURL"
-# get directory listing
 
+boxDirCode=`echo $baseURL | sed 's@.*/\(.*\)$@\1@'`
+echo "Directory code: $boxDirCode"
 
-#"type":"file","id":1917358802,"expirations":[],"canChangeExpiration":false,"tags":[],"description":"","date":1480871800,"extension":"pdf","name":"Carrapatoso, Eurico - Coral A.pdf"
+pageContent=`$CURL -k -L --silent "$baseURL"`
+numPages=`echo $pageContent | grep -Po 'pageCount":[0-9]+,' | sed -n 's/pageCount":\([0-9]*\),/\1/p'`
+echo "Num. pages: $numPages"
 
-gDirCode=`echo $baseURL | sed 's@.*/\(.*\)$@\1@'`
-
-echo $gDirCode
-$CURL -k -L --silent "$baseURL" |
-grep -Po 'typedID":"[^"]+","type":"file","id":[0-9]+,(.+?),"name":"[^"]+"' | # find links
-while read fileInfo
+while [ "$currPage" -le "$numPages" ]
 do
-    echo "File info: $fileInfo"
-    fileCode=`echo $fileInfo | sed -n 's/.*typedID":"\([^"]*\).*/\1/p'` # extract the code for file download (this is how a file is identified in Box)
-    fileName=`echo $fileInfo | sed -n 's/.*"name":"\([^"]\+\)".*/\1/p'` # extract the file name
-    echo "File code: $fileCode"
-    echo "File name: $fileName"
-    linkLine="https://app.box.com/index.php?rm=box_download_shared_file&shared_name=$gDirCode&file_id=$fileCode"
-    outFileName=`echo $fileName | sed 's/\\u[0-9a-f]\{4\}/_/g' | tr ' ' '_'`
-    localFile="$outDir/$outFileName"
-    echo "Local file: $localFile"
+    echo "$pageContent" | 
+    grep -Po 'typedID":"[^"]+","type":"file","id":[0-9]+,(.+?),"name":"[^"]+"' | # find links
+    while read fileInfo
+    do
+        echo "File info: $fileInfo"
+        fileCode=`echo $fileInfo | sed -n 's/.*typedID":"\([^"]*\).*/\1/p'` # extract the code for file download (this is how a file is identified in Box)
+        fileName=`echo $fileInfo | sed -n 's/.*"name":"\([^"]\+\)".*/\1/p'` # extract the file name
+        echo "File code: $fileCode"
+        echo "File name: $fileName"
+        linkLine="https://app.box.com/index.php?rm=box_download_shared_file&shared_name=$boxDirCode&file_id=$fileCode"
+        outFileName=`echo $fileName | sed 's/\\u[0-9a-f]\{4\}/_/g' | tr ' ' '_'`
+        localFile="$outDir/$outFileName"
+        echo "Local file: $localFile"
 
-    $KC_HOME/getRemoteFile.sh "$linkLine" "$localFile"
+        $KC_HOME/getRemoteFile.sh "$linkLine" "$localFile"
 
-    if [ $? -ne 0 ] ; then
-        echo "Having problems contacting Box. Try again in a couple of minutes."
-        exit
+        if [ $? -ne 0 ] ; then
+            echo "Having problems contacting Box. Try again in a couple of minutes."
+            exit
+        fi
+    done
+
+    currPage=$((currPage + 1))
+    if [ "$currPage" -le "$numPages" ] ; then
+        echo "Fetching page $currPage from $baseURL?page=$currPage"
+        pageContent=$($CURL -k -L --silent "$baseURL?page=$currPage")
     fi
+    
 done
