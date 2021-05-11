887d92c4b61083537f7433ef0b9fc8c08257819b
VPeeters <github@victorpeeters.nl>
2021-05-11 22:33:52 +0200

    Include subdirectories OwnCloud (#71)
    Added support for subdirectories when using OwnCloud

--- src/usr/local/kobocloud/getOwncloudList.sh
+++ src/usr/local/kobocloud/getOwncloudList.sh
@@ -10,6 +10,6 @@ echo '<?xml version="1.0"?>
 <a:propfind xmlns:a="DAV:">
 <a:prop><a:resourcetype/></a:prop>
 </a:propfind>' |
-$CURL -k --silent -i -X PROPFIND -u $user: $davServer/public.php/webdav --upload-file - -H "Depth: 1" | # get the listing
+$CURL -k --silent -i -X PROPFIND -u $user: $davServer/public.php/webdav --upload-file - -H "Depth: infinity" | # get the listing
 grep -Eo '<d:href>[^<]*[^/]</d:href>' | # get the links without the folders
 sed 's@</*d:href>@@g'
