a82662fba96ca9441461e4eb839964482cf1244a
VPeeters <github@victorpeeters.nl>
2021-05-13 19:53:57 +0200

    Add function

--- src/usr/local/kobocloud/getGDriveFiles.sh
+++ src/usr/local/kobocloud/getGDriveFiles.sh
@@ -6,6 +6,25 @@ outDir="$2"
 #load config
 . $(dirname $0)/config.sh
 
+find_files() #function to find all files in the folder 
+{
+
+curl -k -L --silent "https://drive.google.com/drive/folders/$1" |
+grep -Eo "\\\x5b\\\x22[^\\\]*\\\x22,\\\x5b\\\x22$1\\\x22\\\x5d\\\n,\\\x22[^\\\]*\\\x22,\\\x22application\\\/[^\\\]*" |
+while read entry
+do
+    entryType=`echo $entry | sed -n 's/.*x22\(.*\)$/\1/p'` #Get the type. Needed to see if it's a file or a folder.
+    entryCode=`echo $entry | sed -n 's/x5bx22\(.*\)x22,x5bx22.*$/\1/p'` #Get the identifying code of the file/folder
+
+    
+    if [ "$fileType" = "application/vnd.google-apps.folder" ]; then #if it's a folder it runs this function for the folder
+        find_files $entryCode
+    else
+        echo $entry | sed -n 's/\([^\\\]*\)x22,.*/\1/p'
+    fi
+done
+}
+
 # get directory listing
 echo "Getting $baseURL"
 # get directory listing
