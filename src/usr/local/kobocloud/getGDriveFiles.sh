70ef50400c8cdf4f4b5817f8664c27067e38477c
VPeeters <github@victorpeeters.nl>
2021-05-15 17:43:05 +0200

    Gdrive subdirectories support (#74)
    * Add subdirectory support for GDrive
    
    * fix mistake
    
    made a small mistake while renaming the function at one point but not the others
    
    * Add function
    
    * Fix mistake
    
    * Keep subdir structure
    
    Keep the folder structure from GDrive on Kobo
    
    * Support special characters in filenames
    
    * Fix using grep -P
    
    grep -P is not supported by Kobo. Using a different solution.
    
    * $CURL instead of curl
    
    By mistake used curl instead of $CURL.
    
    * fix mistake

--- src/usr/local/kobocloud/getGDriveFiles.sh
+++ src/usr/local/kobocloud/getGDriveFiles.sh
@@ -6,6 +6,27 @@ outDir="$2"
 #load config
 . $(dirname $0)/config.sh
 
+find_files() #function to find all files in the folder 
+{
+
+
+$CURL -k -L --silent "https://drive.google.com/drive/folders/$1" |
+grep -Eo "\\\x5b\\\x22[^\\\]*\\\x22,\\\x5b\\\x22$1\\\x22\\\x5d\\\n,\\\x22.{1,250}\\\x22,\\\x22application\\\/[^\\\]*" |
+sed 's/\\\x/\\\\\x/g' |
+while read entry
+do
+    entryType=`echo $entry | sed -n 's/.*\\\x22\(.*\)$/\1/p'` #Get the type. Needed to see if it's a file or a folder.
+    entryCode=`echo $entry | sed -n 's/\\\x5b\\\x22\(.*\)\\\x22,\\\x5b\\\x22.*$/\1/p'` #Get the identifying code of the file/folder
+    entryName=`echo $entry | sed -n 's/\\\x5b\\\x22.*,\\\x22\(.*\)\\\x22,\\\x22application.*$/\1/p'`
+
+    if [ "$entryType" = "application/vnd.google-apps.folder" ]; then #if it's a folder it runs this function for the folder
+        find_files $entryCode "$entryName\/"
+    else
+        echo $entry | sed -n "s/\([^\\\]*\\\x22,.*\\\x22\)\(.*\)\\\x22,\\\x22application.*$/\1$2\\2/p"
+    fi
+done
+}
+
 # get directory listing
 echo "Getting $baseURL"
 # get directory listing
@@ -13,17 +34,19 @@ echo "Getting $baseURL"
 gDirCode=`echo $baseURL | sed 's@.*/\([^/\?]*\).*@\1@'`
 
 echo $gDirCode
-$CURL -k -L --silent "$baseURL" |
-grep -Eo "\\\x5b\\\x22[^\\\]*\\\x22,\\\x5b\\\x22$gDirCode\\\x22\\\x5d\\\n,\\\x22[^\\\]*" | # find links
+
+links=`find_files "$gDirCode"` # find links
+echo "$links" |
+sed 's/\\\x/\\\\\x/g' |
 while read fileInfo
 do
     echo "File info: $fileInfo"
-    fileCode=`echo $fileInfo | sed -n 's/x5bx22\([^\\\]*\)x22,.*/\1/p'` # extract the code for file download (this is how a file is identified in GDrive)
-    fileName=`echo $fileInfo | sed -n 's/.*x22\(.*\)$/\1/p'` # extract the file name
+    fileCode=`echo $fileInfo | sed -n 's/\\\x5b\\\x22\([^\\\]*\)\\\x22,.*/\1/p'` # extract the code for file download (this is how a file is identified in GDrive)
+    fileName=`echo $fileInfo | sed -n 's/.*\\\x22\(.*\)$/\1/p'` # extract the file name
     echo "File code: $fileCode"
     echo "File name: $fileName"
     linkLine="https://drive.google.com/uc?id=$fileCode&export=download"
-    outFileName=`echo $fileName | tr ' ' '_'`
+    outFileName=`/bin/echo -e "$fileName" | tr ' ' '_' `
     localFile="$outDir/$outFileName"
 
     $KC_HOME/getRemoteFile.sh "$linkLine" "$localFile"
