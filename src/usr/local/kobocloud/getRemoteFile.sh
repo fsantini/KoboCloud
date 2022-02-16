ebefc925f6e21d87e14c64bfcbd28113097e9113
fsantini <francesco.santini@gmail.com>
2022-02-16 12:06:33 +0100

    Deal with servers that don't support resume. Fixes #112

--- src/usr/local/kobocloud/getRemoteFile.sh
+++ src/usr/local/kobocloud/getRemoteFile.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+retry="TRUE"
+
+if [ "$1" = "NORETRY" ]
+then
+    retry="FALSE"
+    shift 1
+fi
+
 linkLine="$1"
 localFile="$2"
 user="$3"
@@ -28,7 +36,9 @@ echo "Status: $status"
 echo "Output: "
 cat $outputFileTmp
 
-statusCode=`cat $outputFileTmp | grep 'HTTP/' | tail -n 1 | cut -d' ' -f3`
+statusCode=`grep 'HTTP/' "$outputFileTmp" | tail -n 1 | cut -d' ' -f3`
+grep -q "Cannot resume" "$outputFileTmp"
+errorResume=$?
 rm $outputFileTmp
 
 echo "Remote file information:"
@@ -40,7 +50,24 @@ if echo "$statusCode" | grep -q "403"; then
 fi
 if echo "$statusCode" | grep -q "50.*"; then
     echo "Error: Server error"
-    exit 3
+    if [ $errorResume ] && [ "$retry" = "TRUE" ]
+    then
+        echo "Can't resume. Checking size"
+        contentLength=`$curlCommand -k -sLI "$linkLine" | grep -i 'Content-Length' | sed 's/.*:\s*\([0-9]*\).*/\1/'`
+        existingLength=`stat --printf="%s" "$localFile"`
+        echo "Remote length: $contentLength"
+        echo "Local length: $existingLength"
+        if [ $contentLength = 0 ] || [ $existingLength = $contentLength ]
+        then
+            echo "Not redownloading - Size not available or file already downloaded"
+        else
+            echo "Retrying download"
+            rm "$localFile"
+            $0 NORETRY "$@"
+        fi
+    else
+        exit 3
+    fi
 fi
 
 if grep -q "^REMOVE_DELETED" $UserConfig; then
